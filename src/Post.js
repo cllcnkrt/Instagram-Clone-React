bc1b7e153aaf32db49f0270a3916b412984dd63e
cllcnkrt <cllcnkrt@outlook.com>
2021-08-04 14:19:18 +0300

    post css bug fixed

--- src/Post.js
+++ src/Post.js
@@ -2,26 +2,25 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 
-function Post({username, imageUrl, caption}) {
+function Post({ username, imageUrl, caption }) {
   return (
     <div className="post">
+
       <div className="post__header">
         <Avatar
-          clasname="post__avatar"
+          className="post__avatar"
           alt="Remy Sharp"
           src="/static/images/avatar/1.jpg"
         />
-        <h3>{username}</h3>
+        <h3 >{username}</h3>
       </div>
-      <img
-        className="post__image"
-        src={imageUrl}
-        alt=""
-      />
+
+      <img className="post__image" src={imageUrl} alt="" />
 
       <h4 className="post__text">
         <strong>{username}</strong> {caption}
       </h4>
+
     </div>
   );
 }
