4d14a2bbe71e98001d92a6125a0c0a91bc0de2f5
cllcnkrt <cllcnkrt@outlook.com>
2021-08-04 14:36:04 +0300

    posts coming firestora database

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-function App() {
+import { db } from "./firebase";
 
-  const [posts, setPosts] = useState([
-    {
-      username:"meddison",
-      caption:"Sunday Funday",
-      imageUrl:"https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-    },
-    {
-      username:"meddison",
-      caption:"Sunday Funday",
-      imageUrl:"https://images.pexels.com/photos/1247526/pexels-photo-1247526.jpeg?cs=srgb&dl=pexels-perfecto-capucine-1247526.jpg&fm=jpg"
-    },
-    {
-      username:"Jeniffer",
-       caption:"Coming soon",
-      imageUrl:"https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-    }
-  ])
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
   return (
     <div className="app">
       <div className="app__header">
-        <img
-          className="app__headerImage"
-          src="/images/logo.png"         
-        />
+        <img className="app__headerImage" src="/images/logo.png" />
       </div>
-      {
-        posts.map((post,index) =>(
-          <Post 
-          key={index}
+      {posts.map(({ post, id }) => (
+        <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
-          />
-        ))
-      }
-      
-
+        />
+      ))}
     </div>
   );
 }
